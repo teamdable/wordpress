e6aa6bb2b815f735dd521d5015d67d3d887b48b0
Taegon Kim <gonom9@gmail.com>
2018-09-13 20:04:26 +0900

    Bump to 2.0.1

--- dable-for-wordpress.php
+++ dable-for-wordpress.php
@@ -4,16 +4,16 @@ Plugin Name: Dable for WordPress
 Description: Adds meta tags for Dable and SEO.
 Author: Dable
 Text Domain: dable
-Version: 1.3.4
+Version: 2.0.1
 Author URI: http://dable.io
 Domain Path: /languages
 */
 /**
  * @package dable
- * @version 1.3.4
+ * @version 2.0.1
  */
 
-define( 'DABLE_PLUGIN_VERSION', '1.3.4' );
+define( 'DABLE_PLUGIN_VERSION', '2.0.1' );
 define( 'DABLE_PLUGIN_BASENAME', plugin_basename(__FILE__) );
 define( 'DABLE_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
